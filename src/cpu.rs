73af732f8fc5fc953b10b2fc643e47af57818c66
Brian Lewis <keller@csh.rit.edu>
2019-11-25 19:23:50 +0100

    forgot to collect the output of the parallel map which explained why it was so fast. Still faster but not nearly as much.

--- src/cpu.rs
+++ src/cpu.rs
@@ -6,9 +6,9 @@ use super::izhikevich::Izhikevich;
 pub(crate) fn main(mut neurons: Vec<Izhikevich>, time_steps: usize) {
 
     for _ in 0..time_steps {
-        let _spikes = neurons.par_iter_mut().map(|n| {
+        let _spikes: Vec<bool> = neurons.par_iter_mut().map(|n| {
             n.compute_step()
-        });
+        }).collect();
     }
 }
 
